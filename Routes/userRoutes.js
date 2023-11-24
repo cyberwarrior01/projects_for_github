db7c1ea6698a8c565148794b208bb039dd5801dc
Cyber warrior <ramchandran262004@gmail.com>
2023-11-24 12:20:36 +0530

    user route created

---
+++ Routes/userRoutes.js
@@ -0,0 +1,19 @@
+// Import necessary modules and userController
+const express = require("express");
+const {
+  regsterUser,  // Controller function for user registration
+  loginUser,    // Controller function for user login
+  currentUser,  // Controller function for fetching current user details
+} = require("../Controllers/userController");  // Import userController module
+const router = express.Router();  // Create an Express router
+
+// User registration route
+router.route("/register").post(regsterUser);
+
+// User login route
+router.route("/login").post(loginUser);
+
+// Current user details route
+router.route("/current_user").get(currentUser);
+
+module.exports = router;  // Export the router for use in other parts of the application
